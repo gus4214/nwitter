0a26966151ec09c3733c9398d796c4a5a5a951b4
gus4214 <98795239+gus4214@users.noreply.github.com>
2022-10-02 23:10:01 +0900

    2.6 Log Out

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,15 +1,21 @@
 import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "../routes/Home";
-import Auth from "../routes/Auth";
+import Navigation from "components/Navigation";
+import Profile from "routes/Profile";
+import Home from "routes/Home";
+import Auth from "routes/Auth";
 
 const Routers = ({ isLoggedIn }) => {
   return (
     <BrowserRouter>
+      {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
-          <Route path="/" element={<Home />} />
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+          </>
         ) : (
           <Route path="/" element={<Auth />} />
         )}
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { auth } from "fbase";
+import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+  const onLogOutClick = () => {
+    auth.signOut();
+    navigate("/");
+  };
+
+  return <button onClick={onLogOutClick}>Log Out</button>;
 };
 
 export default Profile;
